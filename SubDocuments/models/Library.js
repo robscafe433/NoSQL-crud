3eadada341788d6928bebd2f1a9ff8d0e6c9c727
Rob <robscafe333@yahoo.com>
2024-11-26 22:28:07 -0800

    working on homework subDocuments, however it is still not working entirely correctly as in Library.js  I am unable to console.log the books array (booksData), right now on the terminal I am getting an empty array. I will continue working on this.

---
+++ SubDocuments/models/Library.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+// TODO: Define a new schema named `bookSchema` for the subdocument
+
+const bookSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+});
+
+const librarySchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	//
+	// TODO: Add the `books` subdocument to the parent document as an array
+	books: [bookSchema],
+	//
+	lastAccessed: { type: Date, default: Date.now },
+});
+
+// TODO: Create a model named `Library`
+
+const LibraryLocations = mongoose.model("LibraryLocations", librarySchema);
+
+// TODO: Create a new instance of the model including the subdocuments
+
+const booksData = [
+	{ name: "Great Expectations" },
+	{ name: "Magpye Murders" },
+	{ name: "Condition Red - New Worlds in Space" },
+];
+
+LibraryLocations.create({ name: "Riverside Library", book: booksData })
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
+
+module.exports = LibraryLocations;
